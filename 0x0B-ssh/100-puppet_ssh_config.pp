3b9e086333e6aa664fdd79caecb148524304de16
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-26 04:17:54 +0100

    Added whitespace before 'echo'

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # A Puppet manifest that makes changes to our configuration file.
-exec {'echo':
+exec { 'echo':
     path    => 'usr/bin:/bin',
     command => 'echo "  IdentityFile ~/.ssh/school\n  PasswordAuthentication no" >> /etc/ssh/ssh_config',
     returns => [0, 1],
