c68cbd1bf09982e92ff7c0a0b8dc94a02b64be3d
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-27 14:50:12 +0100

    Switched to the exec resource

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,6 @@
 # A Puppet manifest to install and configure Nginx on a server
-package { 'puppet':
-    ensure   => 'installed'
-    provider => 'apt',
-}
 
-exec { 'puppetlabs-nginx':
-    commad => 'puppet module install puppetlabs-nginx'
-}
-
-class { 'nginx':
-    ensure  => 'installed',
-    service => 'running',
-}
-
-nginx::resource::vhost { 'dariusdaniel.tech':
-    ensure      => present,
-    www_root    => '/var/www/html',
-    listen_port => '80'
-    index_files => ['index.nginx-debian.html']
+exec {'install':
+  provider => bash,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/google.com permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
