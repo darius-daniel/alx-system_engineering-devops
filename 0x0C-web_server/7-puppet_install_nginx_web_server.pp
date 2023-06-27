e7d51628893e6e3ec5be43888f0750d83b81f6c8
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-27 14:54:53 +0100

    Fixed formatting issues

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # A Puppet manifest to install and configure Nginx on a server
 
-exec {'install':
+exec { 'install':
   provider => bash,
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/google.com permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
