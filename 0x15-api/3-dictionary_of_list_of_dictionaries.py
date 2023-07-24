fc319a3d3b4f9523ffdc1688fad79c5be84e09c3
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-24 15:26:27 +0100

    Modified .gitignore and fixed code to export all data as one json object

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,21 +8,22 @@ if __name__ == "__main__":
     users_info_url = 'https://jsonplaceholder.typicode.com/users/'
     users = json.loads(requests.get(users_info_url).content)
 
+    data = {}
+    for user in users:
+        data['{}'.format(user.get('id'))] = []
+
+        todo_url = (
+            "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+                user.get('id')
+            ))
+        tasks = json.loads(requests.get(todo_url).content)
+
+        for task in tasks:
+            data['{}'.format(user.get('id'))].append({
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': user.get('username')
+            })
+
     with open("todo_all_employees.json", 'w') as f:
-        for user in users:
-            data = {'{}'.format(user.get('id')): []}
-
-            todo_url = (
-                "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-                    user.get('id')
-                ))
-            tasks = json.loads(requests.get(todo_url).content)
-
-            for task in tasks:
-                data['{}'.format(user.get('id'))].append({
-                    'task': task.get('title'),
-                    'completed': task.get('completed'),
-                    'username': user.get('username')
-                })
-
-                json.dump(data, f)
+        json.dump(data, f)
