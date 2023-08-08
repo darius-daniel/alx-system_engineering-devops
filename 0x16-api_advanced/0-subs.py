079a62d3973d2d09ed2caa17abdb6a56af6c784e
Darius Daniel <dariusdaniel771@gmail.com>
2023-08-08 13:12:53 +0100

    Initial commit to project directory

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script containing a function that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers) for a given
+subreddit. If an invalid subreddit is given, the function returns 0.
+"""
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """
+    A function that queries the Reddit API and returns the total number of
+    subscribers for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Chrome'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = json.loads(response.content.decode('utf-8'))
+
+    try:
+        total_subs = params["data"]["subscribers"]
+    except KeyError:
+        return 0
+    else:
+        return total_subs
+
